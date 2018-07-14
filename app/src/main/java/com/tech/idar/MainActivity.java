eda9402886513d0288d1a8e9f1d5ce9f4ce4a72c
idar bondowoso <darmawanramadana01@gmail.com>
2018-07-15 02:22:10 +0700

    Peralatan Idar

---
+++ app/src/main/java/com/tech/idar/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tech.idar;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    ModelCoba mc;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/tech/idar/ModelCoba.java
@@ -0,0 +1,21 @@
+package com.tech.idar;
+
+public class ModelCoba {
+    String nim, nama;
+
+    public ModelCoba() {
+    }
+
+    public ModelCoba(String nim, String nama) {
+        this.nim = nim;
+        this.nama = nama;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+}
